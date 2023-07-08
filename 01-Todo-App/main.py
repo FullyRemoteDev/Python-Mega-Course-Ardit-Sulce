561c5674a645e66f9b88e8d7f398e18a91dd0c76
Jagannath Arumugam <45625449+JagannathArumugam@users.noreply.github.com>
2023-07-08 19:00:58 +0530

    adds ability to edit

--- 01-Todo-App/main.py
+++ 01-Todo-App/main.py
@@ -3,7 +3,7 @@ user_prompt = "Enter a Todo: "
 todos = []
 
 while True:
-    user_action = input("Choose add, show or exit: ")
+    user_action = input("Choose add, show, edit or exit: ")
     user_action = user_action.strip()
 
     match user_action:
@@ -14,6 +14,10 @@ while True:
             for item in todos:
                 item = item.title()
                 print(item)
+        case 'edit':
+            number = int(input("Enter the number of the todo to edit: "))
+            new_todo = input("Enter new todo: ")
+            todos[number - 1] = new_todo
         case 'exit':
             break
         case _:
