8a4e24958dc71824c1763d1dc913300d805c5e49
Jagannath Arumugam <45625449+JagannathArumugam@users.noreply.github.com>
2023-08-07 17:54:01 +0530

    adds modules for functions

--- 01-Todo-App/main.py
+++ 01-Todo-App/main.py
@@ -1,19 +1,5 @@
-def get_todos(filepath="todos.txt"):
-    """
-    Read a text file and return the list of to-do items.
-    """
-    with open(filepath, 'r') as file_local:
-        todos_local = file_local.readlines()
-    return todos_local
-
-
-def write_todos(todos_arg, filepath="todos.txt"):
-    """
-    Write the to-do items list in the text file.
-    """
-    with open(filepath, 'w') as file_local:
-        file_local.writelines(todos_arg)
-
+# from functions import get_todos, write_todos
+import functions
 
 while True:
     user_action = input("Choose add, show, edit, complete or exit: ")
@@ -22,13 +8,13 @@ while True:
     if user_action.startswith('add'):
         todo = user_action[4:]
 
-        todos = get_todos()
+        todos = functions.get_todos()
         todos.append('\n' + todo)
 
-        write_todos(todos)
+        functions.write_todos(todos)
 
     elif user_action.startswith('show'):
-        todos = get_todos()
+        todos = functions.get_todos()
 
         for index, item in enumerate(todos):
             item = item.strip('\n').title()
@@ -38,12 +24,12 @@ while True:
         try:
             edit_item_index = int(user_action[5:])
 
-            todos = get_todos()
+            todos = functions.get_todos()
 
             new_todo = input("Enter new todo: ") + '\n'
             todos[edit_item_index - 1] = new_todo
 
-            write_todos(todos)
+            functions.write_todos(todos)
         except ValueError:
             print("Your command is not valid.")
             continue
@@ -52,12 +38,12 @@ while True:
         try:
             completed_item_index = int(user_action[9:]) - 1
 
-            todos = get_todos()
+            todos = functions.get_todos()
 
             todo_to_remove = todos[completed_item_index].strip('\n')
             todos.pop(completed_item_index)
 
-            write_todos(todos)
+            functions.write_todos(todos)
 
             message = f"\nThe Todo '{todo_to_remove.title()}' was removed from the list\n"
             print(message)
