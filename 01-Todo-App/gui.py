2e6b58883c4d837688aa792d8e928e20c2f85795
Jagannath Arumugam <45625449+JagannathArumugam@users.noreply.github.com>
2023-08-10 17:51:39 +0530

    adds icons instead of text for add, complete buttons and other minor changes

--- 01-Todo-App/gui.py
+++ 01-Todo-App/gui.py
@@ -7,13 +7,13 @@ psg.theme('DarkGrey2')
 label_clock = psg.Text(text='', key='clock')
 label = psg.Text(text="Type in a todo:")
 input_box = psg.InputText(tooltip="Enter todo", key='todo')
-add_button = psg.Button('Add')
+add_button = psg.Button(size=2, image_source='add.png', tooltip="Add Todo", key='add')
 list_box = psg.Listbox(values=functions.get_todos(),
                        key='todos',
                        enable_events=True,
                        size=(36, 9))
 edit_button = psg.Button('Edit')
-complete_button = psg.Button('Complete')
+complete_button = psg.Button(size=4, image_source='complete.png', tooltip="Complete Todo", key='complete')
 exit_button = psg.Button('Exit')
 
 app_layout = [[label_clock],
@@ -31,12 +31,13 @@ while True:
     window['clock'].update(value=time.strftime('%b %d, %Y %H:%M:%S'))
 
     match event:
-        case 'Add':
+        case 'add':
             todos = functions.get_todos()
             new_todo = values['todo'] + '\n'
             todos.append(new_todo)
             functions.write_todos(todos)
             window['todos'].update(values=todos)
+            window['todo'].update(value='')
         case 'Edit':
             try:
                 todo_to_edit = values['todos'][0]
@@ -49,7 +50,7 @@ while True:
                 window['todos'].update(values=todos)
             except IndexError:
                 psg.popup("Please select an item first", font=('Helvetica', 16))
-        case 'Complete':
+        case 'complete':
             try:
                 todo_to_complete = values['todos'][0]
                 todos = functions.get_todos()
