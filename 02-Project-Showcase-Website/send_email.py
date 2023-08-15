e6ba90ddebe2d06a08abe1bfec424ae8e1e75eab
Jagannath Arumugam <45625449+JagannathArumugam@users.noreply.github.com>
2023-08-15 17:32:19 +0530

    adds contact form that can send email

---
+++ 02-Project-Showcase-Website/send_email.py
@@ -0,0 +1,21 @@
+import smtplib
+import ssl
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+def send_email(message):
+    host = 'smtp.gmail.com'
+    port = 465
+
+    username = os.getenv('APP2_USER_EMAIL')
+    password = os.getenv('APP2_USER_PASSWORD')
+
+    receiver = os.getenv('APP2_RECEIVER')
+    context = ssl.create_default_context()
+
+    with smtplib.SMTP_SSL(host, port, context=context) as server:
+        server.login(username, password)
+        server.sendmail(username, receiver, message)
