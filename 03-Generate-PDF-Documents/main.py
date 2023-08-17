13849a02db2ed81cefae24a1fbfb6a36f1e164fe
Jagannath Arumugam <45625449+JagannathArumugam@users.noreply.github.com>
2023-08-17 11:46:09 +0530

    adds ruled pages instead of blank pages

--- 03-Generate-PDF-Documents/main.py
+++ 03-Generate-PDF-Documents/main.py
@@ -14,18 +14,26 @@ for index, row in df.iterrows():
     pdf.cell(w=0, h=12, txt=row['Topic'], align='L', ln=1)
     pdf.line(10, 21, 200, 21)
 
+    # Ruled page
+    for i in range(20, 280, 10):
+        pdf.line(10, i, 200, i)
+
     # Set the footer
     pdf.ln(254)
     pdf.set_font(family='Times', style='I', size=8)
     pdf.set_text_color(180, 180, 180)
     pdf.cell(w=0, h=10, txt=row['Topic'], align='R')
 
-    for i in range(row['Pages'] - 1):
+    for _ in range(row['Pages'] - 1):
         pdf.add_page()
         # Set the footer
         pdf.ln(266)
         pdf.set_font(family='Times', style='I', size=8)
         pdf.set_text_color(180, 180, 180)
         pdf.cell(w=0, h=10, txt=row['Topic'], align='R')
+
+        # Ruled page
+        for i in range(20, 280, 10):
+            pdf.line(10, i, 200, i)
 
 pdf.output('output.pdf')
