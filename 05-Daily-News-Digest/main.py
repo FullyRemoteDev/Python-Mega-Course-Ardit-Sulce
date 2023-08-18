da2a882408df8e2a2d06485a67410af62b6ba941
Jagannath Arumugam <45625449+JagannathArumugam@users.noreply.github.com>
2023-08-18 12:31:07 +0530

    adds links to news items and other refinements

--- 05-Daily-News-Digest/main.py
+++ 05-Daily-News-Digest/main.py
@@ -5,9 +5,10 @@ from dotenv import load_dotenv
 from send_email import send_email
 
 load_dotenv()
+
 news_api = os.getenv('APP5_NEWS_API_KEY')
 news_topic = 'tesla'
-url = (f'https://newsapi.org/v2/everything?q={news_topic}'
+url = (f'https://newsapi.org/v2/everything?q={news_topic}&language=en'
        f'&sortBy=publishedAt&apiKey={news_api}')
 
 # Making the request
@@ -18,10 +19,13 @@ content = request.json()
 articles = content['articles']
 
 # List article titles and descriptions
-body = ''
-for article in articles:
+body = f'Subject: Today\'s news about {news_topic}\n'
+for article in articles[:21]:
     if article['title'] is not None:
-        body = body + article['title'] + '\n' + article['description'] + '\n\n'
+        body = (body
+                + article['title'] + '\n'
+                + article['description'] + '\n'
+                + article['url'] + '\n\n')
 
 body = body.encode('utf-8')
 send_email(message=body)
