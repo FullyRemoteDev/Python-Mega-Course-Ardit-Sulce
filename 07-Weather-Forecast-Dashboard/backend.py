2fbce86390cebe422aa689d4b86dc9f9a49205fe
Jagannath Arumugam <45625449+JagannathArumugam@users.noreply.github.com>
2023-08-22 17:47:52 +0530

    adds functionality to display both temperature and sky conditions with error check

--- 07-Weather-Forecast-Dashboard/backend.py
+++ 07-Weather-Forecast-Dashboard/backend.py
@@ -7,7 +7,7 @@ load_dotenv()
 api_key = os.getenv('APP7_API_KEY')
 
 
-def get_data(place, forecast_days=None, kind=None):
+def get_data(place, forecast_days=None):
     url = f"https://api.openweathermap.org/data/2.5/forecast?q={place}&units=metric&appid={api_key}"
     response = requests.get(url)
     weather_data = response.json()
@@ -15,13 +15,8 @@ def get_data(place, forecast_days=None, kind=None):
     nr_values = forecast_days * 8
     filtered_data = filtered_data[:nr_values]
 
-    if kind == 'Temperature':
-        filtered_data = [dict['main']['temp'] for dict in filtered_data]
-    elif kind == 'Sky':
-        filtered_data = [dict['weather'][0]['main'] for dict in filtered_data]
-
     return filtered_data
 
 
 if __name__ == '__main__':
-    print(get_data(place='Tokyo', forecast_days=3, kind='Temperature'))
+    print(get_data(place='Tokyo', forecast_days=3))
