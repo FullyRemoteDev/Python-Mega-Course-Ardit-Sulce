7b935b799737a3db4ce94bc8e667c0597e838948
Jagannath Arumugam <45625449+JagannathArumugam@users.noreply.github.com>
2023-08-26 17:13:30 +0530

    adds timestamp to the video frame

--- 09-Webcam-Monitoring-Email-Alert/main.py
+++ 09-Webcam-Monitoring-Email-Alert/main.py
@@ -1,14 +1,18 @@
 import cv2
 import time
+from sending_email import send_email
 
 pc_cam_port = 0
-pc_cam = cv2.VideoCapture(pc_cam_port, cv2.CAP_DSHOW)
+pc_cam_video = cv2.VideoCapture(pc_cam_port, cv2.CAP_DSHOW)
 time.sleep(1)
 
 first_frame = None
+objects_list = []
+day_and_time = time.strftime("%A, %d %b %Y %H:%M:%S")
 
 while True:
-    check, frame = pc_cam.read()
+    object_in_view = 0
+    check, frame = pc_cam_video.read()
 
     # Convert Frame to Grayscale - to reduce data size
     gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
@@ -28,23 +32,46 @@ while True:
     # Using dilate to remove noise
     dil_frame = cv2.dilate(thresh_frame, None, iterations=2)
     # Show the processed video feed
-    cv2.imshow('My Video', dil_frame)
+    cv2.imshow("Processed Video Feed", dil_frame)
 
     # Detect the contours around the white areas - new object(s) in the scene
     contours, check2 = cv2.findContours(dil_frame, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
     # Iterate over contours of all the objects in the scene
     for contour in contours:
-        if cv2.contourArea(contour) < 10_000:
+        if cv2.contourArea(contour) < 5_000:
             continue
         # Extracting the coordinates and sizes of the bounding box of the contour
         x, y, w, h = cv2.boundingRect(contour)
         # Draw a bounding box around the contour - including colour and width
-        cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 3)
+        rectangle = cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 3)
+        if rectangle.any():
+            # Object enters the scene (value changes from 0 to 1)
+            object_in_view = 1
 
-    cv2.imshow('Video', frame)
+    # Store all the object intrusions (0 or 1) in the list
+    objects_list.append(object_in_view)
+    # Extract only the last two unique values
+    objects_list = objects_list[-2:]
+
+    # Check if object exits the scene (value changes from 1 to 0)
+    if objects_list[0] == 1 and objects_list[1] == 0:
+        send_email()
+
+    # Add Day and Time overlay on the video feed
+    cv2.putText(frame,
+                day_and_time,
+                (18, 36),
+                cv2.FONT_HERSHEY_SIMPLEX,
+                0.5,
+                (0, 191, 255),
+                1,
+                cv2.LINE_AA
+                )
+    # Show the original video feed
+    cv2.imshow("Original Video Feed", frame)
 
     key = cv2.waitKey(1)
     if key == ord('q'):
         break
 
-pc_cam.release()
+pc_cam_video.release()
