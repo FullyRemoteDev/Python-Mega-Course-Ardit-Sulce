8d4fb14a7b1a2be040f330f35840fff14e8f54bb
Jagannath Arumugam <45625449+JagannathArumugam@users.noreply.github.com>
2023-08-28 21:00:05 +0530

    adds ability to send email when an event is found - runs every hour

--- 10-Music-Event-Web-Scraper/main.py
+++ 10-Music-Event-Web-Scraper/main.py
@@ -1,5 +1,12 @@
 import requests
 import selectorlib
+import smtplib
+import ssl
+import os
+import time
+from dotenv import load_dotenv
+
+load_dotenv()
 
 URL = 'http://programmer100.pythonanywhere.com/tours/'
 HEADERS = {
@@ -20,7 +27,19 @@ def extract_info(source):
     return extracted_content
 
 
-def send_email():
+def send_email(message):
+    host = 'smtp.gmail.com'
+    port = 465
+
+    username = os.getenv('APP10_USER_EMAIL')
+    password = os.getenv('APP10_USER_PASSWORD')
+
+    receiver = os.getenv('APP10_RECEIVER')
+    context = ssl.create_default_context()
+
+    with smtplib.SMTP_SSL(host, port, context=context) as server:
+        server.login(username, password)
+        server.sendmail(username, receiver, message)
     print("Email was sent!")
 
 
@@ -28,16 +47,23 @@ def store_text(extracted):
     with open('data.txt', 'a') as file:
         file.write(extracted + '\n')
 
-def read_text(extracted):
-    with open('data.txt', 'r') as file:
+
+def read_text(data_file):
+    with open(data_file, 'r') as file:
         return file.read()
 
 
 if __name__ == '__main__':
-    scraped_content = scrape_page(URL)
-    tour_info = extract_info(scraped_content)
-    print(tour_info)
-    content = read_text(tour_info)
-    if tour_info not in content:
-        store_text(tour_info)
-        send_email()
+    while True:
+        scraped_content = scrape_page(URL)
+        tour_info = extract_info(scraped_content)
+        print(tour_info)
+
+        content = read_text('data.txt')
+
+        if tour_info != "No upcoming tours":
+            if tour_info not in content:
+                store_text(tour_info)
+                send_email(message="New event was found!")
+
+        time.sleep(3600)
