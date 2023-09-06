5b5d4839697d08880240243af707429b06804f3f
Jagannath Arumugam <45625449+JagannathArumugam@users.noreply.github.com>
2023-09-06 16:47:05 +0530

    adds functionality to connect with openai API, get response and display in UI

--- 15-ChatBot-with-ChatGPT-PyQt/main.py
+++ 15-ChatBot-with-ChatGPT-PyQt/main.py
@@ -1,10 +1,14 @@
 from PyQt6.QtWidgets import QMainWindow, QTextEdit, QLineEdit, QPushButton, QApplication
 import sys
+import threading
+from backend import Chatbot
 
 
 class ChatbotWindow(QMainWindow):
     def __init__(self):
         super().__init__()
+
+        self.chatbot = Chatbot()
 
         self.setMinimumSize(560, 430)
 
@@ -16,16 +20,29 @@ class ChatbotWindow(QMainWindow):
         # Add the input field widget
         self.input_field = QLineEdit(self)
         self.input_field.setGeometry(15, 380, 450, 35)
+        self.input_field.returnPressed.connect(self.send_message)
 
         # Add the button
         self.button = QPushButton('Send', self)
         self.button.setGeometry(475, 380, 75, 35)
+        self.button.clicked.connect(self.send_message)
 
         self.show()
 
+    def send_message(self):
+        user_input = self.input_field.text().strip()
+        user_input = f"<p style='color: #333333'>Me: {user_input}</p>"
+        self.chat_area.append(user_input)
+        self.input_field.clear()
 
-class Chatbot:
-    pass
+        thread = threading.Thread(target=self.get_bot_response, args=(user_input, ))
+        thread.start()
+
+    def get_bot_response(self, user_input):
+        response = self.chatbot.get_response(user_input)
+        response = f"<p style='color: #333333; background-color: #E9E9E9'>Bot: {response}</p>"
+        self.chat_area.append(response)
+
 
 
 if __name__ == '__main__':
