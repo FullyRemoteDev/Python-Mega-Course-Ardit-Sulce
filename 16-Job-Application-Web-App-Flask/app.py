599e1afa4b266bcd7251f0fb5811fa7bf262d570
Jagannath Arumugam <45625449+JagannathArumugam@users.noreply.github.com>
2023-09-07 11:28:21 +0530

    adds a basic database schema and functionality to get the user data from form submission

--- 16-Job-Application-Web-App-Flask/app.py
+++ 16-Job-Application-Web-App-Flask/app.py
@@ -1,11 +1,42 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from flask_sqlalchemy import SQLAlchemy
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 app = Flask(__name__)
 
+app.config['SECRET_KEY'] = os.getenv('APP16_SECRET_KEY')
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
+db = SQLAlchemy(app)
 
-@app.route('/')
+
+class Form(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    first_name = db.Column(db.String(80))
+    last_name = db.Column(db.String(80))
+    email_id = db.Column(db.String(80))
+    start_date = db.Column(db.Date)
+    occupation = db.Column(db.String(80))
+
+
+@app.route('/', methods=['GET', 'POST'])
 def index():
+    if request.method == 'POST':
+        first_name = request.form['first_name']
+        last_name = request.form['last_name']
+        email_id = request.form['email_id']
+        start_date = request.form['date']
+        occupation = request.form['occupation']
+        # print([first_name, last_name, email_id, start_date, occupation])
+
+    elif request.method == 'GET':
+        pass
+
     return render_template('index.html')
 
-
-app.run(debug=True, port=5001)
+if __name__ == '__main__':
+    with app.app_context():
+        db.create_all()
+        app.run(debug=True, port=5001)
