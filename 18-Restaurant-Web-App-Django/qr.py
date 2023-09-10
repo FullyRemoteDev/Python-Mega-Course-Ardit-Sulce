ad9159fc4a2afcaab389b67c397bfe859a3a95ee
Jagannath Arumugam <45625449+JagannathArumugam@users.noreply.github.com>
2023-09-10 17:43:18 +0530

    adds db model, views, admin interface, dynamic links and bootstrap styling

--- 18-Restaurant-Web-App-Django/qr.py
+++ 18-Restaurant-Web-App-Django/qr.py
@@ -1,4 +1,11 @@
 import qrcode
+import os
+from dotenv import load_dotenv
 
-image = qrcode.make('https://127.0.0.1:8000')
+load_dotenv()
+
+app18_ip = os.getenv('APP18_WEB_APP_HOST_IP')
+app18_port = 8000
+app18_url = f"http://{app18_ip}:{app18_port}"
+image = qrcode.make(app18_url)
 image.save('qr.png')
